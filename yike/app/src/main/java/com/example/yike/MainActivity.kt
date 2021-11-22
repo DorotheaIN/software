eceb9cee9a4da7a119b8003e8d6caa13c1bf3570
Tours <tours0@qq.com>
2021-11-22 21:53:27 +0800

    first commit-dzy

--- yike/app/src/main/java/com/example/yike/MainActivity.kt
+++ yike/app/src/main/java/com/example/yike/MainActivity.kt
@@ -3,11 +3,9 @@ package com.example.yike
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.yike.ui.theme.YikeTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +13,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             YikeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = "login",
+                    route = "root"
+                ) {
+                    composable("question") {
+                        QuestionScreen()
+                    }
+                    composable("welcome") {
+                        WelcomeScreen(navController)
+                    }
+                    composable("login") {
+                        LoginScreen(navController)
+                    }
+                    composable("discuss") {
+//                        val homeViewModel: HomeViewModel = hiltNavGraphViewModel()
+//                        val uiState by homeViewModel.uiState.collectAsState()
+//                        val plantList = homeViewModel.pagedPlants
+//
+//                        HomeScreen(uiState = uiState) { plant ->
+//                            navController.navigate("plant/${plant.id}")
+//                        }
+                        DiscussScreen(navController)
+                    }
+//                    composable(
+//                        route = "plant/{id}",
+//                        arguments = listOf(navArgument("id") { type = NavType.IntType })
+//                    ) {
+//                        val plantViewModel: QuestionViewModel = hiltNavGraphViewModel()
+//                        val plant: Plant by plantViewModel.plantDetails.collectAsState(Plant(0))
+//
+//                        QuestionScreen(plant = plant)
+//                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    YikeTheme {
-        Greeting("Android")
     }
 }
